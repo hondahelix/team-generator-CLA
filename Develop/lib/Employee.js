7edcc3362375031dcc1d6d3b9bcf1260a4e1926a
Jonathan Honda <hondahelix120@gmail.com>
2020-10-12 18:07:14 -0700

    cleared all employee tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name,id,email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    //ask about formatting this like this
+    getName(){return this.name;};
+    getId(){return this.id};
+    getEmail(){return this.email};
+    getRole(){return Employee.name;}
+}
+module.exports = Employee;
